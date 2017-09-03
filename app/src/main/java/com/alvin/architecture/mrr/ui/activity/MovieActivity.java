207d67657dc5fd1523eb3a15f6265adbe5763018
alvin <fengyu17lee@gmail.com>
2017-09-03 17:07:11 +0800

    movie

---
+++ app/src/main/java/com/alvin/architecture/mrr/ui/activity/MovieActivity.java
@@ -0,0 +1,29 @@
+package com.alvin.architecture.mrr.ui.activity;
+
+import com.alvin.architecture.mrr.ui.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/9/3.
+ */
+
+public class MovieActivity extends BaseActivity {
+    @Override
+    protected int layoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
